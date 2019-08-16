74178fdf576c1a3765b7a57911eee9d60091bd2c
abek <abekdev@163.com>
2019-08-16 18:27:02 +0800

    0008

--- python3/0008_String_to_Integer(atoi).py
+++ python3/0008_String_to_Integer(atoi).py
@@ -2,44 +2,59 @@
 # -*- coding: utf-8 -*-
 # author: abekthink
 
+import re
+
+MAX_INT = 2 ** 31 - 1
+MIN_INT = -2 ** 31
+
 
 class Solution:
+    # def myAtoi(self, s: str) -> int:
+    #     if not s:
+    #         return 0
+    #     idx = 0
+    #     while idx < len(s) and s[idx] == ' ':
+    #         idx += 1
+    #     if not s[idx:]:
+    #         return 0
+    #
+    #     valid_sign = {'+', '-'}
+    #     valid_digits = set([str(i) for i in range(10)])
+    #
+    #     sign = None
+    #     if s[idx] in valid_sign:
+    #         sign = s[idx]
+    #         idx += 1
+    #     elif s[idx] not in valid_digits:
+    #         return 0
+    #
+    #     num = 0
+    #     ln = len(s)
+    #     while idx < ln and s[idx] in valid_digits:
+    #         num = num * 10 + int(s[idx])
+    #         idx += 1
+    #
+    #     if sign == '-':
+    #         num = -num
+    #
+    #     if num > MAX_INT:
+    #         return MAX_INT
+    #     elif num < MIN_INT:
+    #         return MIN_INT
+    #     return num
+
     def myAtoi(self, s: str) -> int:
-        if not s:
-            return 0
-        idx = 0
-        while idx < len(s) and s[idx] == ' ':
-            idx += 1
-        if not s[idx:]:
+        p = re.compile(r'^\s*([+\-]?\d+)')
+        m = p.match(s)
+        if m is None:
             return 0
 
-        valid_sign = {'+', '-'}
-        valid_digits = set([str(i) for i in range(10)])
-
-        sign = None
-        if s[idx] in valid_sign:
-            sign = s[idx]
-            idx += 1
-        elif s[idx] not in valid_digits:
-            return 0
-
-        num = 0
-        ln = len(s)
-        while idx < ln and s[idx] in valid_digits:
-            num = num * 10 + int(s[idx])
-            idx += 1
-
-        if sign == '-':
-            num = -num
-
-        max_int = 2 ** 31 - 1
-        min_int = -2 ** 31
-        if num > max_int:
-            return max_int
-        elif num < min_int:
-            return min_int
-        else:
-            return num
+        num = int(m.group(0))
+        if num > MAX_INT:
+            return MAX_INT
+        elif num < MIN_INT:
+            return MIN_INT
+        return num
 
 
 def main():
