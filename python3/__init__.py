87cc0ffa97d378c522d62816c962c7822dfe9b2f
abek <abekdev@163.com>
2019-08-21 17:39:46 +0800

    common utils

--- python3/__init__.py
+++ python3/__init__.py
@@ -11,6 +11,7 @@ from typing import List
 from collections import defaultdict
 
 
+# Playground for linked-list
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -46,3 +47,14 @@ def listNodeToString(node):
         result += str(node.val) + ", "
         node = node.next
     return "[" + result[:-2] + "]"
+
+
+def prettyPrintLinkedList(node):
+    while node and node.next:
+        print(str(node.val) + "->", end='')
+        node = node.next
+
+    if node:
+        print(node.val)
+    else:
+        print("Empty LinkedList")
