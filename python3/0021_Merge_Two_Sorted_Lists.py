a5ec00232e748f8a2569a8b9935428d4bac65035
abek <abekdev@163.com>
2019-08-21 21:39:51 +0800

    update 0021

--- python3/0021_Merge_Two_Sorted_Lists.py
+++ python3/0021_Merge_Two_Sorted_Lists.py
@@ -6,15 +6,26 @@ from python3 import ListNode, stringToListNode, listNodeToString
 
 
 class Solution:
+    # def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    #     tail = dummy = ListNode(0)
+    #     while l1 or l2:
+    #         if l2 is None or (l1 and l1.val < l2.val):
+    #             tail.next, l1 = l1, l1.next
+    #         else:
+    #             tail.next, l2 = l2, l2.next
+    #         tail = tail.next
+    #     return dummy.next
+
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        tail = new_list = ListNode(0)
-        while l1 or l2:
-            if l2 is None or (l1 and l1.val < l2.val):
+        tail = dummy = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
                 tail.next, l1 = l1, l1.next
             else:
                 tail.next, l2 = l2, l2.next
             tail = tail.next
-        return new_list.next
+        tail.next = l1 or l2
+        return dummy.next
 
 
 def main():
